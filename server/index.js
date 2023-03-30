16ab97853a2e6c2e3d1540e245a1894795cef9df
Jeffrey Neal <jeffneal11@gmail.com>
2023-03-30 16:42:06 -0500

    wireframed app

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
